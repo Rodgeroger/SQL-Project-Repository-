26d1a7448c3979f1fbb8b6712d7ecaa9b288f5c3
Rodrigo Isidro <182361412+Rodgeroger@users.noreply.github.com>
2025-05-20 14:36:34 -0700

    Add files via upload

---
+++ SQL Practice/SQL Youtube Home Page.sql
@@ -0,0 +1,32 @@
+CREATE TABLE HomePage_Videos (
+	video_id int,
+	title varchar(255),
+	channel_name varchar(255),
+	view_count int,
+	like_count int,
+	dislike_count int,
+	category varchar(255),
+	comments int,
+	watched bit	
+	duration TIME,
+	release_time smalldatetime
+	);
+
+INSERT INTO HomePage_Videos (video_id, title, channel_name, view_count, like_count, dislike_count, category, comments, watched, duration, release_time)
+VALUES (1, 'Revisiting Star Wars Battlefront 2 - Luke Reacts', 'Luke Stephens LIVE', 37309, 1400, 38, 'Gaming', 292, 0, '00:15:23', '2025-05-18 00:00')
+
+INSERT INTO HomePage_Videos (video_id, title, channel_name, view_count, like_count, dislike_count, category, comments, watched, duration, release_time)
+VALUES (2, 'The Cold & Calculated Murder Of Valeria Marquez | The Most Dangerous CJNG Enforcer | El Doble RR', 'Disturbed Reality', 2384, 251, 4, 'Horror', 63, 0, '00:15:24', '2025-05-18 00:00')
+
+INSERT INTO HomePage_Videos (video_id, title, channel_name, view_count, like_count, dislike_count, category, comments, watched, duration, release_time)
+VALUES (3, 'Schaffrillas Productions: The Inhibitor Chip Apologist', 'J.J. Plagiarisms', 30000, 1000, 97, 'Video Essay', 1234, 0, '01:04:20', '2024-10-19 00:00')
+
+INSERT INTO HomePage_Videos (video_id, title, channel_name, view_count, like_count, dislike_count, category, comments, watched, duration, release_time)
+VALUES (4, '3 Disturbing TRUE Suburban Horror Stories', 'Mr. Nightmare', 21557, 3000, 0, 'Horror', 523, 0, '00:21:54', '2025-05-20 00:00')
+
+INSERT INTO HomePage_Videos (video_id, title, channel_name, view_count, like_count, dislike_count, category, comments, watched, duration, release_time)
+VALUES (5, 'The FBI "Scam" That Caught 7 Congressmen [Atrioc Reacts]', 'Atrioc VODs', 16000, 383, 2, 'React', 46, 1, '02:44:47', '2025-05-18 00:00')
+
+INSERT INTO HomePage_Videos (video_id, title, channel_name, view_count, like_count, dislike_count, category, comments, watched, duration, release_time)
+VALUES (6, 'BATTLEFRONT IS SO BACK', 'NLTILIA', 882, 0, 0, 'Gaming', 13, 0, '00:12:14', '2025-05-19 00:00')
+
